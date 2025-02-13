fccc9d413345b90dc7091a2f6e19d111ff3e542d
nagamaniponala <ponalanagamani12@gmail.com>
2025-02-13 23:22:48 +0530

    day1,2

--- day2-basic code/main.tf
+++ day2-basic code/main.tf
@@ -1,1 +1,5 @@
-
+resource "aws_instance" "name" {
+  ami=var.ami_id
+  instance_type = "t2.micro"
+  key_name = var.key
+}
---
+++ day2-basic code/variable.tf
@@ -0,0 +1,19 @@
+
+variable "ami_id" {
+    description = "inserting ami value"
+    type = string
+     default = ""
+
+}
+
+variable "type" {
+    type = string
+     default = ""
+
+}
+
+variable "key" {
+    type = string
+     default = ""
+
+}
